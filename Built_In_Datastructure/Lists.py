d8869489f73adb0dcf363cac6bc5e26406b3b927
abdulnahid-aws <abdul.nahid@gmail.com>
2025-12-19 20:02:33 +0530

    mod_lists1

--- Built_In_Datastructure/Lists.py
+++ Built_In_Datastructure/Lists.py
@@ -1,1 +1,10 @@
-lists1 = [ ]
+lists1 = [1,2,3,'ABDUL' ]
+#append() ,extend() and insert()
+lists1.append((4,9))
+lists1.extend( (2,0) )
+lists1.insert(3,'NAHID')
+#del , pop() , remove()
+del lists1[0]
+a=lists1.pop(2)
+print(a)
+print(lists1)
